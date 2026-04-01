0308153134fe268f408024f7b6a922155f96a03a
Gaurav Chauhan <gaurav.chauhan@freshworks.com>
2026-04-01 21:26:57 +0530

    feat: update Next.js configuration and refactor page components
    - Added GitHub Actions support with dynamic basePath and assetPrefix in next.config.ts.
    - Set output to "export" and enabled trailingSlash.
    - Refactored page components (Home, About, Contact, Work) to remove draft mode dependency, fetching data with a fixed parameter.
    - Replaced anchor tags with Link components from Next.js for better routing in WorkSection and TopNav components.

---
+++ out/_next/static/chunks/app/contact/page-d555a05186a6dea5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220,774,977],{3104:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,255,358],()=>e(e.s=3104)),_N_E=e.O()}]);
